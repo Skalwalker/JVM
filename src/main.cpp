fcedcb90dd99d8c0f3773521d25ad9b885354131
Renato Avellar Nobre <rekanobre@gmail.com>
2019-10-19 15:37:40 -0300

    Fixed #25 Implementing CLI

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    string file_name;
+    string path = "test_examples/";
+    // Check the number of parameters
+    if (argc == 1) {
+      file_name = path + "HelloWorld.class";
+    } else if (argc == 2){
+      file_name = path + argv[1];
+    }
+
+
     FILE * fp;
 
-    fp = fopen("test_examples/HelloWorld.class", "rb");
+    fp = fopen(file_name.c_str(), "rb");
     if (fp != NULL) {
         ClassFile classFile(fp);
         Printer printer;
